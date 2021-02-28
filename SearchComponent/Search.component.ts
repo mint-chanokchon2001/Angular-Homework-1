6796064505b7507c4e959d71a908be7befa0897f
mint-chanokchon2001 <79015780+mint-chanokchon2001@users.noreply.github.com>
2021-02-28 21:38:14 +0700

    Add files via upload

---
+++ SearchComponent/Search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'SearchComponent',
+    templateUrl: './Search.component.html',
+    styleUrls: ['./Search.component.css']
+})
+
+export class SearchComponent {
+
+    constructor() {}
+}
