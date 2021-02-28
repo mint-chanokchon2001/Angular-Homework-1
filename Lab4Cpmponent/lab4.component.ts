074cd2279eb0f5a9fb939bd4a9a9ea8e56a6b7b2
mint-chanokchon2001 <79015780+mint-chanokchon2001@users.noreply.github.com>
2021-02-28 21:29:11 +0700

    Lab 4 ทดสอบ Directives

---
+++ Lab4Cpmponent/lab4.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'Lab4Component',
+    templateUrl: './lab4.component.html',
+    styleUrls: ['./lab4.component.css']
+})
+
+export class Lab4Component {
+
+    numbers:number[] = [1,2,3,4,5,6,7,8,9,10];
+;
+    constructor() {}
+}
