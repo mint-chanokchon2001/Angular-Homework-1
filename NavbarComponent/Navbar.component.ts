5b8875acae8051a95a884f5511f67e2584264dbe
mint-chanokchon2001 <79015780+mint-chanokchon2001@users.noreply.github.com>
2021-02-28 21:33:55 +0700

    Add files via upload

---
+++ NavbarComponent/Navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'NavbarComponent',
+    templateUrl: './Navbar.component.html',
+    styleUrls: ['./Navbar.component.css']
+})
+
+export class NavbarComponent {
+
+    constructor() {}
+}
